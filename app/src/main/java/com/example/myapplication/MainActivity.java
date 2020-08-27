b2fcb5905072ad97f14a94967ab9c12c1524c607
Tyrorn <50643248+Tyrorn@users.noreply.github.com>
2020-08-27 15:06:29 +1200

    game Prior to using gridView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+//lol
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
